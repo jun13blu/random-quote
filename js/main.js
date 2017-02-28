68b98d8188f3f5be790e1c0ff73cb5b93ac89c3b
June <jeff.bluesky13@gmail.com>
2017-02-28 13:58:24 +0800

    edited js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
+
+var text = "abcde";
+var textArr = Array.from(text);
+function toSpan(target, input){
+    var x = 0;
+    input.forEach(function(val){
+        $(target).append("<span id='letter-" + x + "'>" + val + "</span>");
+        x++;
+    });
+}
+
+function rndText(len) {
+    var arr = [];
+    var rndArr = [];
+    for(var i = 0; i < len.length; i++){
+        arr.push(i);
+    }
+    for(var i = 0; i < len.length; i++){
+        var rn = Math.floor(Math.random() * arr.length);
+        var num = "#letter-" + arr.splice(rn, 1);
+        $(num).fadeTo("slow", 1);
+    }
+}
+
 $(function(){
     $("button").click(function(){
-        $("h1").fadeOut(300);
+        $("h1").text("");
+        toSpan("h1", textArr);
+        $("h1").fadeTo(1, 0);
+        rndText(textArr);
     });
 });
